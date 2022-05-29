ce42103d1f8fcef7dbb603e894800f7b8b0239e9
Sasha K <a.kosak.mbx@gmail.com>
2022-05-29 16:29:16 +0300

    `mtl::traits::list` module added.

---
+++ mtl/traits/list.test.cpp
@@ -0,0 +1,34 @@
+#include <type_traits>
+#include <catch2/catch.hpp>
+#include <mtl/traits/list.hpp>
+
+
+
+TEST_CASE("List length is the same as number of its elements.") {
+    CHECK(mtl::traits::list<>::length == 0);
+    CHECK(mtl::traits::list<void>::length == 1);
+    CHECK(mtl::traits::list<void, void>::length == 2);
+}
+
+TEST_CASE("`contains` is `true` if the specified element is in the list, `false` otherwise.") {
+    CHECK_FALSE(mtl::traits::list<>::contains<void>);
+    CHECK(mtl::traits::list<void>::contains<void>);
+    CHECK_FALSE(mtl::traits::list<void>::contains<int>);
+    CHECK(mtl::traits::list<void, int, int>::contains<void>);
+    CHECK(mtl::traits::list<int, void, int>::contains<void>);
+    CHECK(mtl::traits::list<int, int, void>::contains<void>);
+    CHECK_FALSE(mtl::traits::list<int, int, int>::contains<void>);
+}
+
+TEST_CASE("`next` contains list with the following set of elements or `void` if last or empty.") {
+    CHECK(std::is_same<typename mtl::traits::list<>::next, void>::value);
+    CHECK(std::is_same<typename mtl::traits::list<int>::next, void>::value);
+    CHECK(std::is_same<typename mtl::traits::list<int, char>::next, mtl::traits::list<char>>::value);
+}
+
+TEST_CASE("`type` is of the same type as the first element in the list or `void` if empty.") {
+    CHECK(std::is_same<typename mtl::traits::list<>::type, void>::value);
+    CHECK(std::is_same<typename mtl::traits::list<int>::type, int>::value);
+    CHECK(std::is_same<typename mtl::traits::list<int, char>::type, int>::value);
+    CHECK(std::is_same<typename mtl::traits::list<int, char>::next::type, char>::value);
+}
