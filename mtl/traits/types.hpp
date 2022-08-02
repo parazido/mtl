9235cfabcef5f039de7226c3b513a881403f8816
Sasha K <a.kosak.mbx@gmail.com>
2022-08-02 22:13:50 +0300

    Type picking was added to the `mtl::types` module.

--- mtl/traits/types.hpp
+++ mtl/traits/types.hpp
@@ -12,6 +12,8 @@ namespace mtl::traits {
             constexpr static uint8_t width = v_width;
         };
 
+        template <uint64_t v_value> struct min_integral_for_value { constexpr static uint64_t value = v_value; };
+
         template <uint8_t v_width> struct min_floating { constexpr static uint8_t width = v_width; };
     } // namespace typesp
 
@@ -33,6 +35,14 @@ namespace mtl::traits {
         template <uint8_t v_width> struct use_min_floating_type {
             using type = typename std::conditional<(v_width <= 23), float, double>::type;
         };
+
+        template <uint64_t v_value> struct use_min_integral_type_for_value {
+            using type = typename std::conditional<
+                v_value <= 0xff, uint8_t,
+                typename std::conditional<
+                    v_value <= 0xffff, uint16_t,
+                    typename std::conditional<v_value <= 0xffffffff, uint32_t, uint64_t>::type>::type>::type;
+        };
     } // namespace detail
 
     struct types {
@@ -44,6 +54,10 @@ namespace mtl::traits {
 
         template <uint8_t v_width> struct pick<typesp::min_floating<v_width>> {
             using type = typename detail::use_min_floating_type<v_width>::type;
+        };
+
+        template <uint64_t v_value> struct pick<typesp::min_integral_for_value<v_value>> {
+            using type = typename detail::use_min_integral_type_for_value<v_value>::type;
         };
     };
 } // namespace mtl::traits
